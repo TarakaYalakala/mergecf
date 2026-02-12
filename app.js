8ac381beb420b4eeeac2259f414541c13e4b7a49
unknown <tarakayalakala@gmail.com>
2026-02-12 06:09:27 +0530

    Merge remote-tracking branch 'origin/main' into feat-2

--- app.js
+++ app.js
@@ -6,13 +6,3 @@
  const array = [1,2,3,4,5];
  const map = array.map((item) => item * 2);
  console.log(map);
-
- // Added map //
-
- const array = [1,2,3,4,5];
- const map = array.map((item) => item * 2);
- console.log(map);
-
- const array = [1,2,3,4,5];
- console.log(array);
- 
